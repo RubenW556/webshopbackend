6292cbbe80e35434f130b597642cc44776ca6d88
RubenW556 <85158519+RubenW556@users.noreply.github.com>
2022-01-17 19:40:36 +0100

    Add files via upload

---
+++ webshop/webshop/src/main/java/com/s1127833/webshop/WebshopApplication.java
@@ -0,0 +1,13 @@
+package com.s1127833.webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopApplication.class, args);
+	}
+
+}
---
+++ webshop/webshop/src/test/java/com/s1127833/webshop/WebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s1127833.webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
